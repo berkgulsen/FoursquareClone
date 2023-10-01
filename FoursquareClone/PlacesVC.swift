7ca09f4b32c08596d79c9acaff668f5b9f3d99a0
berkgulsen <bipurpie.exe@gmail.com>
2023-10-01 23:59:52 +0300

    +logout button added +remember login function added

--- FoursquareClone/PlacesVC.swift
+++ FoursquareClone/PlacesVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class PlacesVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
@@ -16,9 +17,24 @@ class PlacesVC: UIViewController {
         // Do any additional setup after loading the view.
         
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: UIBarButtonItem.Style.plain, target: self, action: #selector(logoutButtonClicked))
     }
 
     @objc func addButtonClicked(){
         //segue
+    }
+    
+    @objc func logoutButtonClicked(){
+        PFUser.logOutInBackground { error in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert,animated: true,completion: nil)
+            } else {
+                self.performSegue(withIdentifier: "toSignUpVC", sender: nil)
+            }
+        }
     }
 }
