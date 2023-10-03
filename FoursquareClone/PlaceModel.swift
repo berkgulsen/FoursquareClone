e8ae970bdf129c3d988c9f78049791224bc54df4
berkgulsen <bipurpie.exe@gmail.com>
2023-10-03 22:57:59 +0300

    +photo choosing +default photo controls

---
+++ FoursquareClone/PlaceModel.swift
@@ -0,0 +1,23 @@
+//
+//  PlaceModel.swift
+//  FoursquareClone
+//
+//  Created by Berk Gülşen on 3.10.2023.
+//
+
+import Foundation
+import UIKit
+
+class PlaceModel {
+    static let sharedInstance = PlaceModel()
+    
+    var placeName = ""
+    var placeType = ""
+    var placeAtmosphere = ""
+    var placeImage = UIImage()
+    
+    private init(){
+        
+    }
+    
+}
--- FoursquareClone/addPlaceVC.swift
+++ FoursquareClone/addPlaceVC.swift
@@ -7,21 +7,79 @@
 
 import UIKit
 
-class addPlaceVC: UIViewController {
+class addPlaceVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var placeNameText: UITextField!
     @IBOutlet weak var placeTypeText: UITextField!
     @IBOutlet weak var placeAtmosphereText: UITextField!
     @IBOutlet weak var placeImageView: UIImageView!
     
+    var isImageDefault = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        placeImageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        placeImageView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+       
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
     @IBAction func nextButtonClicked(_ sender: Any) {
+        guard let placeName = placeNameText.text,
+              let placeType = placeTypeText.text,
+              let placeAtmosphere = placeAtmosphereText.text else {
+            return
+        }
+        
+        if placeName.isEmpty || placeType.isEmpty || placeAtmosphere.isEmpty {
+            let alert = UIAlertController(title: "Error", message: "Place name/type/atmosphere cannot be empty.", preferredStyle: .alert)
+            let okButton = UIAlertAction(title: "OK", style: .default)
+            alert.addAction(okButton)
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        if isImageDefault {
+            let alert = UIAlertController(title: "Error", message: "Please select an image for the place.", preferredStyle: .alert)
+            let okButton = UIAlertAction(title: "OK", style: .default)
+            alert.addAction(okButton)
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        // Eğer varsayılan fotoğraf ile aynı değilse işlemi devam ettir
+        PlaceModel.sharedInstance.placeName = placeName
+        PlaceModel.sharedInstance.placeType = placeType
+        PlaceModel.sharedInstance.placeAtmosphere = placeAtmosphere
+        PlaceModel.sharedInstance.placeImage = placeImageView.image!
         performSegue(withIdentifier: "toMapVC", sender: nil)
     }
     
-
+    
+    @objc func chooseImage() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[.originalImage] as? UIImage {
+            placeImageView.image = selectedImage
+            
+            // Seçilen fotoğraf varsayılan fotoğraf ile aynı mı kontrol et
+            let defaultImage = UIImage(named: "taphere")
+            isImageDefault = selectedImage.isEqual(defaultImage)
+        }
+        
+        self.dismiss(animated: true)
+    }
 }
