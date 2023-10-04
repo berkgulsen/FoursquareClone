fbfb6d0546db23cc3eb28f474e2eb2e565d4d142
berkgulsen <bipurpie.exe@gmail.com>
2023-10-04 21:39:44 +0300

    showing current location on map

--- FoursquareClone/mapVC.swift
+++ FoursquareClone/mapVC.swift
@@ -8,9 +8,11 @@
 import UIKit
 import MapKit
 
-class mapVC: UIViewController {
+class mapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    var isLocationUpdated = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,24 @@ class mapVC: UIViewController {
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(title: "Save", style: UIBarButtonItem.Style.plain, target: self, action: #selector(saveButtonClicked))
         
         navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "Back", style: UIBarButtonItem.Style.plain, target: self, action: #selector(backButtonClicked))
+        
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first, !isLocationUpdated{
+            let span = MKCoordinateSpan(latitudeDelta: 0.015, longitudeDelta: 0.015)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+            isLocationUpdated = true
+            locationManager.stopUpdatingLocation()
+        }
+    }
+
     
     @objc func saveButtonClicked(){
         //parse
