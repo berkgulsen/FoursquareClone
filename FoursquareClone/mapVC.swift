07dc5da086881754742247ba5248a161e8451d24
berkgulsen <bipurpie.exe@gmail.com>
2023-10-04 22:18:42 +0300

    saving all information to Parse

--- FoursquareClone/mapVC.swift
+++ FoursquareClone/mapVC.swift
@@ -7,14 +7,13 @@
 
 import UIKit
 import MapKit
+import Parse
 
 class mapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     var locationManager = CLLocationManager()
     var isLocationUpdated = false
-    var chooserLongitude = ""
-    var chooserLatitude = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,7 +45,31 @@ class mapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     }
     
     @objc func saveButtonClicked() {
-        //parse
+        
+        let placeModel = PlaceModel.sharedInstance
+        
+        let object = PFObject(className: "Places")
+        object["name"] = placeModel.placeName
+        object["type"] = placeModel.placeType
+        object["atmosphere"] = placeModel.placeAtmosphere
+        object["latitude"] = placeModel.placeLatitude
+        object["longitude"] = placeModel.placeLongitude
+        
+        if let imageData = placeModel.placeImage.jpegData(compressionQuality: 0.5) {
+            object["image"] = PFFileObject(name: "image.jpeg", data: imageData)
+        }
+        
+        object.saveInBackground { success, error in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+                alert.addAction(okButton)
+                self.present(alert, animated: true)
+            } else {
+                self.performSegue(withIdentifier: "fromMapVCtoPlacesVC", sender: nil)
+            }
+        }
+        
     }
     
     @objc func backButtonClicked() {
@@ -64,8 +87,8 @@ class mapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
             annotation.subtitle = PlaceModel.sharedInstance.placeType
             mapView.addAnnotation(annotation)
             
-            self.chooserLatitude = String(coordinate.latitude)
-            self.chooserLongitude = String(coordinate.longitude)
+            PlaceModel.sharedInstance.placeLatitude = String(coordinate.latitude)
+            PlaceModel.sharedInstance.placeLongitude = String(coordinate.longitude)
         }
     }
 }
