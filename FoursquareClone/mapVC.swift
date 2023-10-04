5cd3104392dba7826ccf9ba50c9f67698cd30973
berkgulsen <bipurpie.exe@gmail.com>
2023-10-04 21:58:03 +0300

    annotation added with long press

--- FoursquareClone/mapVC.swift
+++ FoursquareClone/mapVC.swift
@@ -13,6 +13,8 @@ class mapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
     var locationManager = CLLocationManager()
     var isLocationUpdated = false
+    var chooserLongitude = ""
+    var chooserLatitude = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,10 +29,14 @@ class mapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(_:)))
+        longPressGesture.minimumPressDuration = 3.0
+        mapView.addGestureRecognizer(longPressGesture)
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first, !isLocationUpdated{
+        if let location = locations.first, !isLocationUpdated {
             let span = MKCoordinateSpan(latitudeDelta: 0.015, longitudeDelta: 0.015)
             let region = MKCoordinateRegion(center: location.coordinate, span: span)
             mapView.setRegion(region, animated: true)
@@ -38,13 +44,28 @@ class mapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
             locationManager.stopUpdatingLocation()
         }
     }
-
     
-    @objc func saveButtonClicked(){
+    @objc func saveButtonClicked() {
         //parse
     }
     
-    @objc func backButtonClicked(){
+    @objc func backButtonClicked() {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func handleLongPress(_ gestureRecognizer: UILongPressGestureRecognizer) {
+        if gestureRecognizer.state == .began {
+            let touchPoint = gestureRecognizer.location(in: mapView)
+            let coordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = PlaceModel.sharedInstance.placeName
+            annotation.subtitle = PlaceModel.sharedInstance.placeType
+            mapView.addAnnotation(annotation)
+            
+            self.chooserLatitude = String(coordinate.latitude)
+            self.chooserLongitude = String(coordinate.longitude)
+        }
     }
 }
