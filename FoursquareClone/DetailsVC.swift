676219a0eda48dc8412e669245f4957737fd40e0
berkgulsen <bipurpie.exe@gmail.com>
2023-10-05 19:12:00 +0300

    user forwared for directions when annotation tapped

--- FoursquareClone/DetailsVC.swift
+++ FoursquareClone/DetailsVC.swift
@@ -9,24 +9,28 @@ import UIKit
 import MapKit
 import Parse
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController, MKMapViewDelegate {
+    
     @IBOutlet weak var detailsImageView: UIImageView!
     @IBOutlet weak var detailsNameLabel: UILabel!
     @IBOutlet weak var detailsTypeLabel: UILabel!
     @IBOutlet weak var detailsAtmosphereLabel: UILabel!
+    @IBOutlet weak var detailsMapView: MKMapView!
+    
     
     var choosenPlaceId = ""
-    
     var choosenLongitude = Double()
     var choosenLatitude = Double()
     
-    @IBOutlet weak var detailsMapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
         getDataFromParse()
+        
+        detailsMapView.delegate = self
     }
     
     func getDataFromParse() {
@@ -83,6 +87,42 @@ class DetailsVC: UIViewController {
                         annotation.subtitle = self.detailsTypeLabel.text
                         self.detailsMapView.addAnnotation(annotation)
                                     
+                    }
+                }
+            }
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId)
+        
+        if pinView == nil {
+            pinView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView?.canShowCallout = true
+            let button = UIButton(type: UIButton.ButtonType.detailDisclosure)
+            pinView?.rightCalloutAccessoryView = button
+        } else {
+            pinView?.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if self.choosenLatitude != 0.0, self.choosenLongitude != 0.0 {
+            let requestLocation = CLLocation(latitude: self.choosenLatitude, longitude: self.choosenLongitude)
+            
+            CLGeocoder().reverseGeocodeLocation(requestLocation) { placemarks, error in
+                if let placemark = placemarks {
+                    if placemark.count > 0 {
+                        let mkPlacemark = MKPlacemark(placemark: placemark[0])
+                        let mapItem = MKMapItem(placemark: mkPlacemark)
+                        mapItem.name = self.detailsNameLabel.text
+                        mapItem.openInMaps(launchOptions: [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving])
                     }
                 }
             }
